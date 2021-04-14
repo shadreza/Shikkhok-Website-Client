78602d67a456eeaa772205e25c7f50787b09e2cb
shadreza <45334134+shadreza@users.noreply.github.com>
2021-04-15 02:36:36 +0600

    done upto routes

---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import './SearchBox.css';
+
+const SearchBox = () => {
+    return (
+        <div className="searchBoxMainDiv">
+            <input className="inputBoxWritingPart" type="text" placeholder="type your product name"/>
+            <Button className="searchButton" variant="contained" color="primary">
+                Search
+            </Button>
+        </div>
+    );
+};
+
+export default SearchBox;
--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../header/Header';
+import SearchBox from '../SearchBox/SearchBox';
 import './Homepage.css'
 
 const Homepage = () => {
     return (
         <div>
             <Header></Header>
+            <SearchBox></SearchBox>
         </div>
     );
 };
