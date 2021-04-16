889f84cd14799e72ec31be07be00376a0db27fa4
shadreza <45334134+shadreza@users.noreply.github.com>
2021-04-17 00:21:12 +0600

    Done With ImageBB

--- src/components/AdminPage-Add/AdminPageAdd.js
+++ src/components/AdminPage-Add/AdminPageAdd.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import { Button } from '@material-ui/core';
+import axios from 'axios';
+import React, { useState } from 'react';
 import './AdminPageAdd.css';
 
 const AdminPageAdd = () => {
+
+    let Prd = {
+        namePrd : "",
+        pricePrd: 0.0,
+        quantityPrd: 0,
+        imageUrlPrd: ""
+    }
+
+    const [addingPrd , setAddingPrd] = useState (Prd);
+    const addingButtonClicked = () => {
+        const name = document.getElementById("inputName").value;
+        const qty = document.getElementById("inputQuantity").value;
+        const price = document.getElementById("inputPrice").value;
+        let image="";
+        const img = document.getElementById("inputImage").files[0];
+        console.log(img);
+        const imgData = new FormData();
+        imgData.set('key','45993c2fa3b2590d51cee87d8ff551a6');
+        imgData.append('image',img);
+        axios.post('https://api.imgbb.com/1/upload',imgData)
+        .then(function (response) {
+            image = response.data.data.display_url;
+            Prd.namePrd=name;
+            Prd.pricePrd=price;
+            Prd.quantityPrd=qty;
+            Prd.imageUrlPrd=image;
+            setAddingPrd(Prd);
+            console.log(Prd);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    }
     return (
-        <div>
-            admin page to add
+        <div className="adminPageAddMainDiv"> 
+            <div className="addingProduct">
+                <p><strong>Name Of The Product</strong></p>
+                <input id="inputName" className="inputBox" type="text" placeholder="input name"/>
+                <p><strong>Quantity Of The Product</strong></p>
+                <input id="inputQuantity" className="inputBox" type="number" placeholder="input quantity"/>
+                <p><strong>Price Of The Product</strong></p>
+                <input id="inputPrice" className="inputBox" type="number" placeholder="input price"/>
+                <br/><br/>
+                <p><strong>Upload Your Product Image</strong></p>
+                <input id="inputImage" className="inputBox" type="file" placeholder="upload image"/>
+                <br/><br/>
+                <Button variant="contained" color="primary" onClick={() =>addingButtonClicked()}>
+                    Add Product
+                </Button>
+            </div>
         </div>
     );
 };
--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -1,6 +1,13 @@
 import { Button } from '@material-ui/core';
 import React, { useContext, useEffect } from 'react';
-import { HeaderStayingContext, UserContext } from '../../App';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import { UserContext } from '../../App';
+import { HeaderStayingContext} from '../../App';
 import AdminPageAdd from '../AdminPage-Add/AdminPageAdd';
 import AdminPageManage from '../AdminPage-Manage/AdminPageManage';
 import LoginPage from '../LoginPage/LoginPage';
@@ -8,41 +15,42 @@ import './AdminPage.css';
 const AdminPage = () => {
 
     const user = useContext(UserContext);
-    let managePartSelected = true;
-
-    const handleManageButton = () => {
-        console.log('true');
-        managePartSelected = true;
-    }
-
-    const handleAddButton = () => {
-        console.log('false');
-        managePartSelected = false;
-    }
-    
 
     return (
-        <div className="adminPageMainDiv">
-            {
-                 user[0].isLoggedInOrNot === false ? <LoginPage></LoginPage>
-                 :
-                <div className="adminDiv">
-                    <div className="topPart">
-                        <Button variant="contained" color="primary" onClick={() => handleManageButton()}>
-                            Manage Product
-                        </Button>
-                            
-                        <Button variant="contained" color="primary" onClick={() => handleAddButton()}>
-                            Add Product
-                        </Button>                        
-                    </div>       
-                
-                    <div className="bottomPart" id="bottom">
-                        
+        <Router>
+            <div className="adminPageMainDiv">
+                {
+                    // user[0].isLoggedInOrNot === false ? <LoginPage></LoginPage>
+                    // :
+                    <div className="adminDiv">
+                        <div className="topPart">
+                            <Link to='/adminManagePage'>
+                                <Button variant="contained" color="primary">
+                                    Manage Product
+                                </Button>
+                            </Link>
+                            <Link to='/adminAddPage'>
+                                <Button variant="contained" color="primary">
+                                    Add Product
+                                </Button>
+                            </Link>                       
+                        </div>       
+                    
+                        <div className="bottomPart" id="bottom">
+
+                        </div>
                     </div>
-                </div>
-            }
-        </div>
+                }
+            </div>
+            <Switch>
+                <Route path="/adminManagePage">
+                    <AdminPageManage></AdminPageManage>
+                </Route>
+                <Route path="/adminAddPage">
+                    <AdminPageAdd></AdminPageAdd>
+                </Route>
+            </Switch>
+        </Router>
     );
 };
 
