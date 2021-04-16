58ec70c4ebbba8e77301e6d403efd66f81f0be2e
shadreza <45334134+shadreza@users.noreply.github.com>
2021-04-17 04:34:43 +0600

    added ad prd

--- src/components/AdminPage-Add/AdminPageAdd.js
+++ src/components/AdminPage-Add/AdminPageAdd.js
@@ -1,59 +1,63 @@
 import { Button } from '@material-ui/core';
 import axios from 'axios';
 import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
 import './AdminPageAdd.css';
 
 const AdminPageAdd = () => {
 
-    let Prd = {
-        namePrd : "",
-        pricePrd: 0.0,
-        quantityPrd: 0,
-        imageUrlPrd: ""
-    }
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
-    const [addingPrd , setAddingPrd] = useState (Prd);
-    const addingButtonClicked = () => {
-        const name = document.getElementById("inputName").value;
-        const qty = document.getElementById("inputQuantity").value;
-        const price = document.getElementById("inputPrice").value;
+    const onSubmit = data => {
+        let Prd = {
+            namePrd : data.nameOfPrd,
+            pricePrd: data.priceOfPrd,
+            quantityPrd: data.qytOfPrd,
+            imageUrlPrd: data.imgOfPrd
+        }
         let image="";
-        const img = document.getElementById("inputImage").files[0];
-        console.log(img);
+        const img = Prd.imageUrlPrd[0];
         const imgData = new FormData();
         imgData.set('key','45993c2fa3b2590d51cee87d8ff551a6');
         imgData.append('image',img);
         axios.post('https://api.imgbb.com/1/upload',imgData)
         .then(function (response) {
             image = response.data.data.display_url;
-            Prd.namePrd=name;
-            Prd.pricePrd=price;
-            Prd.quantityPrd=qty;
             Prd.imageUrlPrd=image;
-            setAddingPrd(Prd);
             console.log(Prd);
+            const url = "http://localhost:5055/addProduct";
+            axios.post(url,Prd)
+            .then(res=>{
+                console.log(res);
+                console.log("Successful");
+            })
+            .catch(err=>{
+                console.log(err.message);
+            })                                                                      
         })
         .catch(function (error) {
             console.log(error);
         });
-
+        
     }
+
     return (
-        <div className="adminPageAddMainDiv"> 
-            <div className="addingProduct">
-                <p><strong>Name Of The Product</strong></p>
-                <input id="inputName" className="inputBox" type="text" placeholder="input name"/>
-                <p><strong>Quantity Of The Product</strong></p>
-                <input id="inputQuantity" className="inputBox" type="number" placeholder="input quantity"/>
-                <p><strong>Price Of The Product</strong></p>
-                <input id="inputPrice" className="inputBox" type="number" placeholder="input price"/>
-                <br/><br/>
-                <p><strong>Upload Your Product Image</strong></p>
-                <input id="inputImage" className="inputBox" type="file" placeholder="upload image"/>
-                <br/><br/>
-                <Button variant="contained" color="primary" onClick={() =>addingButtonClicked()}>
-                    Add Product
-                </Button>
+        <div className="adminPageAddMainDiv">
+            <div className="addingProduct"> 
+                <form className="addingProduct" onSubmit={handleSubmit(onSubmit)}>
+                    <p><strong>Name Of The Product</strong></p>
+                    <input id="inputName" placeholder="input name" className="inputBox" {...register("nameOfPrd" , { required: true })} /> 
+                    <p><strong>Quantity Of The Product</strong></p>
+                    <input id="inputQuantity" className="inputBox" type="number" placeholder="input quantity" {...register("qytOfPrd" , { required: true })} />
+                    <p><strong>Price Of The Product</strong></p>
+                    <input id="inputPrice" className="inputBox" type="number" placeholder="input price" {...register("priceOfPrd" , { required: true })}/>
+                    <br/><br/>
+                    <p><strong>Upload Your Product Image</strong></p>
+                    <input id="inputImage" className="inputBox" type="file" placeholder="upload image" {...register("imgOfPrd" , { required: true })} />
+                    <br/><br/>
+                    {errors.exampleRequired && <span>This field is required</span>}
+                    <input type="submit" />
+                </form>
             </div>
         </div>
     );
